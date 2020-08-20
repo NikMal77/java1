4298bd6482ef6d70f3e275b4a9a470042d9c8d9b
Nik <malovskiy@list.ru>
2020-08-20 11:19:08 +0500

    Progwards unit 7

--- src/ru/progwards/java1/lessons/bitsworld/SumBits.java
+++ src/ru/progwards/java1/lessons/bitsworld/SumBits.java
@@ -2,13 +2,13 @@ package ru.progwards.java1.lessons.bitsworld;
 
 public class SumBits {
     public static void main(String[] args) {
-        System.out.println(sumBits((byte) 37));
+        System.out.println(sumBits((byte) 127));
     }
 
     public static int sumBits(byte value){
         int sum = 0;
 
-        while (value > 0){
+        for (int i = 0; i < 8; i++) {
             sum += value & 1;
             value = (byte) (value >> 1);
         }
